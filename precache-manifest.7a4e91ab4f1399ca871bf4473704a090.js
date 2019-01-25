8cc5e9ad578a9af2e0da4a00823840bb0e8a8ad2
zmichgen <zmichgen@gmail.com>
2019-01-25 11:30:51 +0300

    Updates

---
+++ precache-manifest.7a4e91ab4f1399ca871bf4473704a090.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd8001780f013eb8d0fb",
+    "url": "/CodeJam-5/static/js/runtime~main.cd800178.js"
+  },
+  {
+    "revision": "50f3f41040ceb2637fa4",
+    "url": "/CodeJam-5/static/js/main.50f3f410.chunk.js"
+  },
+  {
+    "revision": "93526a8f7bd2ec7d0103",
+    "url": "/CodeJam-5/static/js/1.93526a8f.chunk.js"
+  },
+  {
+    "revision": "50f3f41040ceb2637fa4",
+    "url": "/CodeJam-5/static/css/main.a451e127.chunk.css"
+  },
+  {
+    "revision": "93526a8f7bd2ec7d0103",
+    "url": "/CodeJam-5/static/css/1.c0b4228c.chunk.css"
+  },
+  {
+    "revision": "399ee6aabd85d7f0c7a2133f627ee0ab",
+    "url": "/CodeJam-5/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/CodeJam-5/precache-manifest.7a4e91ab4f1399ca871bf4473704a090.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/CodeJam-5/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
